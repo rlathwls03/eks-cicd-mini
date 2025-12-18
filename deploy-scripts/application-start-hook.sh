e8455a2548d4b7168c9ec8c46ac33362d265ac5e
rlathwls03 <ksj3551613@naver.com>
2025-12-18 17:06:24 +0900

    fix server bug

--- deploy-scripts/application-start-hook.sh
+++ deploy-scripts/application-start-hook.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sudo service nginx restart
+APP_DIR=/home/ec2-user/app/backend/build/libs
+
+echo "Stopping existing Spring Boot app..."
+pkill -f 'java -jar' || true
+
+echo "Starting Spring Boot app..."
+cd $APP_DIR
+nohup java -jar *.jar > app.log 2>&1 &
