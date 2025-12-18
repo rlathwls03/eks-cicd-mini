18aaaf80bb233cee620a14cf8ad62be331735bb5
rlathwls03 <ksj3551613@naver.com>
2025-12-18 17:23:22 +0900

    update appspec.yml

--- deploy-scripts/application-start-hook.sh
+++ deploy-scripts/application-start-hook.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 APP_DIR=/home/ec2-user/app/backend/build/libs
+JAR_NAME=backend-0.0.1-SNAPSHOT.jar
 
-echo "Stopping existing Spring Boot app..."
-pkill -f 'java -jar' || true
+# 기존 실행 중인 서버 종료
+pkill -f $JAR_NAME || true
 
-echo "Starting Spring Boot app..."
-cd $APP_DIR
-nohup java -jar *.jar > app.log 2>&1 &
+# 서버 실행 (백그라운드)
+nohup java -jar $APP_DIR/$JAR_NAME > /home/ec2-user/app/app.log 2>&1 &
+
+# nginx 재시작
+sudo systemctl restart nginx
